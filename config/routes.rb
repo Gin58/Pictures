4334097de9a78a158355817ee99f42d9c02e791f
Gin58 <invincible.gnj1875@gmail.com>
2019-11-21 16:24:30 +0900

    コントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :accounts
+  devise_for :accounts, controllers: {
+    registrations: 'accounts/registrations',
+    sessions: 'accounts/sessions'
+  }
+
+  devise_scope :account do
+    get "sign_in", to: 'accounts/sessions#new'
+    get "sign_out", to: 'accounts/sessions#destroy'
+  end
+
   root to: 'home#index'
   get 'home/index'
   get 'home/show'
